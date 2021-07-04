0fba5c41e1d0d8d4158c9088a518072936f78235
Nicolas Grobelny <miki4920@gmail.com>
2021-07-04 19:08:03 +0100

    Implemented multi-dimensional noise

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import math
 import random
-from matplotlib import pyplot as plt
-gradients = [random.uniform(-1, 1) for _ in range(0, 100)]
+
+from itertools import product
+from PIL import Image
 
 
 def fade(t):
@@ -12,16 +13,52 @@ def lerp(a, b, t):
     return a + t * (b-a)
 
 
-def perlin_noise_1d(p):
-    p0 = math.floor(p)
-    p1 = p0+1
-    return lerp(gradients[p0]*(p-p0), gradients[p1]*(p-p1), fade(p-p0))
+class PerlinNoise(object):
+    def __init__(self, dimension):
+        self.dimension = dimension
+        self.gradient = {}
+
+    def get_gradient(self):
+        random_point = [random.gauss(0, 1) for _ in range(self.dimension)]
+        scale = sum(n*n for n in random_point)**-0.5
+        return tuple(point*scale for point in random_point)
+
+    def get_noise(self, point):
+        grid_coordinates = []
+        for coordinate in point:
+            minimum_coordinate = math.floor(coordinate)
+            maximum_coordinate = minimum_coordinate+1
+            grid_coordinates.append((minimum_coordinate, maximum_coordinate))
+
+        dot_products = []
+        for grid_point in product(*grid_coordinates):
+            if grid_point not in self.gradient:
+                self.gradient[grid_point] = self.get_gradient()
+            gradient = self.gradient[grid_point]
+            dot_product = 0
+            for i in range(0, self.dimension):
+                dot_product += gradient[i] * (point[i]-grid_point[i])
+            dot_products.append(dot_product)
+
+        dimension = self.dimension
+        while len(dot_products) > 1:
+            dimension -= 1
+            interpolation = fade(point[dimension] - grid_coordinates[dimension][0])
+
+            future_dot_products = []
+            while dot_products:
+                future_dot_products.append(lerp(dot_products.pop(0), dot_products.pop(0), interpolation))
+            dot_products = future_dot_products
+        return (dot_products[0]+1)/2
 
 
+perlin_generator = PerlinNoise(2)
 
-terrain = [0]*900
-for i in range(0, 900):
-    terrain[i] = perlin_noise_1d(i/10) + perlin_noise_1d(i/20)*2 + perlin_noise_1d(i/40)*4 + perlin_noise_1d(i/80)*8
-print(terrain)
-plt.plot(terrain)
-plt.show()
+terrain = [0]*1000000
+for y in range(0, 1000):
+    for x in range(0, 1000):
+        terrain[y*1000+x] = int(perlin_generator.get_noise((x/100, y/100))*255)
+
+image = Image.new("L", (1000, 1000))
+image.putdata(terrain)
+image.save("terrain.png")
